db099a245b42834ab40669fb47dfc1ff06d7ce30
ed system on behalf of Tyson Thomas <ed>
2021-09-16 21:47:01 +1000

    Scaffold update by instructor

---
+++ calendar.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+
+
+def run():
+    # YOUR CODE HERE
+    pass
+
+if __name__ == '__main__':
+    run()
+
---
+++ daemon.py
@@ -0,0 +1,27 @@
+#!/bin/python
+
+import signal
+import os
+import sys
+
+
+#Use this variable for your loop
+daemon_quit = False
+
+#Do not modify or remove this handler
+def quit_gracefully(signum, frame):
+    daemon_quit = True
+
+
+
+def run():
+    #Do not modify or remove this function call
+    signal.signal(signal.SIGINT, quit_gracefully)
+
+    # Call your own functions from within 
+    # the run() funcion
+    pass
+
+
+if __name__ == '__main__':
+    run()
